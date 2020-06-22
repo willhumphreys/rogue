e3edb6c26893288c36dc4d348ab5a5603198462c
Will Humphreys <whumphreys@gmail.com>
2020-06-22 09:58:28 +0200

    Initial commit

---
+++ src/Monster.js
@@ -0,0 +1,35 @@
+import Entity from "./Entity";
+
+class Monster extends Entity {
+  action(verb, world) {
+    if (verb === "bump") {
+      //attack
+      world.addToHistory(`Player attacks ${this.attributes.name}!`);
+      this.attributes.health = this.attributes.health - 1;
+      if (this.attributes.health <= 0) {
+        world.addToHistory(`${this.attributes.name} dies!`);
+        world.remove(this);
+      } else {
+        world.addToHistory(
+          `${this.attributes.name}'s health = ${this.attributes.health}`
+        );
+        world.player.attributes.health = world.player.attributes.health - 1;
+        if (world.player.attributes.health <= 0) {
+          world.addToHistory("You have died!");
+        } else {
+          world.addToHistory(
+            `You have ${world.player.attributes.health} health`
+          );
+        }
+      }
+    }
+  }
+
+  move(dx, dy) {
+    if (this.attributes.health <= 0) return;
+    this.x += dx;
+    this.y += dy;
+  }
+}
+
+export default Monster;
---
+++ src/ReactRogue.js
@@ -0,0 +1,78 @@
+import React, { useRef, useEffect, useState } from "react";
+import InputManager from "./InputManager";
+import Player from "./Player";
+import World from "./World";
+import Spawner from "./Spawner";
+
+const ReactRogue = ({ width, height, tilesize }) => {
+  const canvasRef = useRef();
+  const [player, setPlayer] = useState(new Player(1, 2, tilesize));
+  const [world, setworld] = useState(new World(width, height, tilesize));
+
+  let inputManager = new InputManager();
+
+  const handleInput = (action, data) => {
+    console.log(`handle input: ${action}:${JSON.stringify(data)}`);
+    let newWorld = new World();
+    Object.assign(newWorld, world);
+    newWorld.movePlayer(data.x, data.y);
+    newWorld.moveMonster(data.x, data.y);
+    setworld(newWorld);
+  };
+
+  useEffect(() => {
+    console.log("Create Map!");
+
+    let newWorld = new World();
+    Object.assign(newWorld, world);
+    newWorld.createCellularMap();
+    newWorld.moveToSpace(world.player);
+    let spawner = new Spawner(newWorld);
+    spawner.spawnLoot(10);
+    spawner.spawnMonsters(6);
+    spawner.spawnStairs();
+    setworld(newWorld);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  useEffect(() => {
+    console.log("Bind input");
+    inputManager.bindkeys();
+    inputManager.subscribe(handleInput);
+    return () => {
+      inputManager.unbindKeys();
+      inputManager.unsubscribe(handleInput);
+    };
+  });
+
+  useEffect(() => {
+    console.log("Draw to canvas");
+    const ctx = canvasRef.current.getContext("2d");
+    ctx.clearRect(0, 0, width * tilesize, height * tilesize);
+    world.draw(ctx);
+  });
+
+  return (
+    <>
+      <canvas
+        ref={canvasRef}
+        width={width * tilesize}
+        height={height * tilesize}
+        style={{ border: "1px solid black", background: "Dimgray" }}
+      ></canvas>
+      <ul>
+        {world.player.inventory.map((item, index) => (
+          <li key={index}>{item.attributes.name}</li>
+        ))}
+      </ul>
+
+      <ul>
+        {world.history.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default ReactRogue;
